bf9435ddbf72b118241d6dade341668a7f44bcd3
TranDTann <trandtan070301@gmail.com>
2025-06-27 01:29:34 +0700

    feature/home-page: create featured post, categories list

--- src/sections/home/view/HomeView.tsx
+++ src/sections/home/view/HomeView.tsx
@@ -1,7 +1,16 @@
-"use client";
+'use client'
+
+import CategoriesBlock from '../CategoriesBlock/CategoriesBlock'
+import { FeaturedPosts } from '../FeaturedPosts'
+import './styles.css'
 
 const HomeView = () => {
-  return <div>Home page</div>;
-};
+  return (
+    <div className="home-container">
+      <FeaturedPosts />
+      <CategoriesBlock />
+    </div>
+  )
+}
 
-export default HomeView;
+export default HomeView
