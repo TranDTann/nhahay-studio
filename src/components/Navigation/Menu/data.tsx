03eb4f67c892bc3d1a49f6ba6d853a52a4166eb9
TranDTann <trandtan070301@gmail.com>
2025-07-27 23:37:25 +0700

    feat/home-page: add lazy load component, update Menu data

--- src/components/Navigation/Menu/data.tsx
+++ src/components/Navigation/Menu/data.tsx
@@ -1,89 +1,31 @@
+import { Category } from '@/store/categories/crud'
 import Link from 'next/link'
 
-export const MENU_ITEMS = [
+export const getMenu = (categories: Category[]) => [
   {
     key: '/home',
     label: <Link href="/home">Trang chủ</Link>
   },
   {
     key: '/categories',
-    label: (
-      <Link style={{ color: 'rgb(34 34 34 / 88%)' }} href="/">
-        Danh mục sản phẩm
-      </Link>
-    ),
-    children: [
-      {
-        key: '1',
-        label: (
-          <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-            <img
-              src="https://hangnhatchuan365.com/wp-content/uploads/2020/09/thiet-bi-bep-108x108-1-36x36.png"
-              alt="Thiết bị nhà bếp"
-              width={20}
-              height={20}
-            />
-            <span>Thiết bị nhà bếp</span>
-          </div>
-        )
-      },
-      {
-        key: '2',
-        label: (
-          <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-            <img
-              src="https://hangnhatchuan365.com/wp-content/uploads/2020/09/tu-lanh-108x108-1-36x36.png"
-              alt="Lưu trữ & làm mát"
-              width={20}
-              height={20}
-            />
-            <span>Lưu trữ & làm mát</span>
-          </div>
-        )
-      },
-      {
-        key: '3',
-        label: (
-          <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-            <img
-              src="https://hangnhatchuan365.com/wp-content/uploads/2020/09/1-1594532749-7006133703-ecovacs-deebot-n79-icon-1-36x36.jpg"
-              alt="Thiết bị làm sạch"
-              width={20}
-              height={20}
-            />
-            <span>Thiết bị làm sạch</span>
-          </div>
-        )
-      },
-      {
-        key: '4',
-        label: (
-          <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-            <img
-              src="https://hangnhatchuan365.com/wp-content/uploads/2020/09/images-1-36x36.png"
-              alt="Phòng tắm & WC"
-              width={20}
-              height={20}
-            />
-            <span>Phòng tắm & WC</span>
-          </div>
-        )
-      },
-      {
-        key: '4',
-        label: (
-          <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-            <img
-              src="https://hangnhatchuan365.com/wp-content/uploads/2020/09/phan-mem-phu-tung-linh-kien-oto-36x36.jpg"
-              alt="Phụ kiện khác"
-              width={20}
-              height={20}
-            />
-            <span>Phụ kiện khác</span>
-          </div>
-        )
-      }
-    ]
+    label: 'Danh mục sản phẩm',
+    children: categories.map((categoryItem) => ({
+      key: categoryItem.id,
+      label: (
+        <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+          <img
+            src={
+              categoryItem?.['thumbnail'] ??
+              'https://hangnhatchuan365.com/wp-content/uploads/2020/09/thiet-bi-bep-108x108-1-36x36.png'
+            }
+            alt={categoryItem.name}
+            width={20}
+            height={20}
+          />
+          <span>{categoryItem.name}</span>
+        </div>
+      )
+    }))
   },
   {
     key: '/blog',
