38dc2068e4e395d0996af1e7558b2bf524667305
TranDTann <trandtan070301@gmail.com>
2025-10-09 00:26:33 +0700

    home-page: add advertisement

--- src/sections/home/RecentPosts/RecentPostsAdvertising/RecentPostsAdvertising.tsx
+++ src/sections/home/RecentPosts/RecentPostsAdvertising/RecentPostsAdvertising.tsx
@@ -1,13 +1,41 @@
+import { useAdvertisementStore } from '@/store/advertisement/advertisementStore'
+import Link from 'next/link'
+import { useEffect } from 'react'
 import './styles.css'
 
 const Advertising = () => {
+  const { getAdvertisements, advertisements } = useAdvertisementStore()
+
+  useEffect(() => {
+    if (advertisements?.length) {
+      return
+    }
+
+    const fetchData = async () => {
+      try {
+        await getAdvertisements()
+      } catch (error) {
+        // Error is already handled in the store
+      }
+    }
+    fetchData()
+  }, [getAdvertisements])
+
+  if (!advertisements?.length) {
+    return null
+  }
+
+  const advertisementView = advertisements[0]
+
   return (
     <div className="recent-post_advertising-image-container image-hover-zoom-container">
-      <img
-        src="https://www.jaybranding.com/wp-content/uploads/2023/06/su-dung-anh-quang-cao-1200x900-v4.jpg"
-        alt="anh-quang-cao"
-        className="recent-post_advertising-image image-hover-zoom"
-      />
+      <Link href={advertisementView.link} target="_blank">
+        <img
+          src={advertisementView.imageUrl}
+          alt="anh-quang-cao"
+          className="recent-post_advertising-image image-hover-zoom"
+        />
+      </Link>
     </div>
   )
 }
