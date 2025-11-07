ac92fb456f1078c1947c0e9d959b7f8c3f261ab8
TranDTann <trandtan070301@gmail.com>
2025-11-08 02:37:34 +0700

    category-page: update UI

--- src/sections/category/CategoryInfo/CategoryInfo.tsx
+++ src/sections/category/CategoryInfo/CategoryInfo.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { Category } from '@/store/categories/crud'
-import './styles.css'
+import { Flex } from 'antd'
+import './styles.scss'
 
 type TCategoryInfoProps = {
   categoryData: Category
@@ -9,17 +10,30 @@ type TCategoryInfoProps = {
 
 const CategoryInfo = ({ categoryData }: TCategoryInfoProps) => {
   return (
-    <div className="category-info-container">
-      <img
-        src={
-          categoryData.urlThumbnail
-            ? categoryData.urlThumbnail
-            : 'https://hangnhatchuan365.com/wp-content/uploads/2020/09/thiet-bi-bep-108x108-1-36x36.png'
-        }
-        alt="category-thumbnail"
-        className="category-thumbnail-image"
-      />
-      <h2 className="category-name">{categoryData.name}</h2>
+    <div id="CategoryInfo">
+      <div className="category-info-container">
+        <div className="category-intro-web">
+          <p>
+            Nhà Hay Studio là trang web chuyên chia sẻ về NHÀ và những thứ liên
+            quan gắn liền với nhà: Smart home, bếp, vật liệu
+          </p>
+        </div>
+        <Flex className="category-info-header" align="center" gap={16}>
+          <img
+            src={
+              categoryData.urlThumbnail
+                ? categoryData.urlThumbnail
+                : 'https://hangnhatchuan365.com/wp-content/uploads/2020/09/thiet-bi-bep-108x108-1-36x36.png'
+            }
+            alt="category-thumbnail"
+            className="category-thumbnail-image"
+          />
+          <div>
+            <p className="category-intro">Bạn đang xem các bài viết về</p>
+            <h2 className="category-name">{categoryData.name}</h2>
+          </div>
+        </Flex>
+      </div>
     </div>
   )
 }
